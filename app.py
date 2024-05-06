263eb574e399dc9d1adbf058ca5b2bbd1115fe32
ObeOne-KMITL <73372869+ObeOne-KMITL@users.noreply.github.com>
2024-05-06 19:50:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,23 @@ app = FastAPI()
 
 @app.get('/')
 def hello_world():
-    return "Hello,World"
+    return {"message": "Hello,World"}
+
+@app.get("/result/{score}")
+
+async def result_exam(score):
+
+    score = int(score)
+
+    if(score >= 50):
+
+        result = "Pass"
+
+    else:    
+
+        result = "No pass"
+
+    return {"your result is": result}
 
 
 if __name__ == '__main__':

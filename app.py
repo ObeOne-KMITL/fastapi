713cdf5f651e1593b506318d6f7c08f267bea02b
ObeOne-KMITL <73372869+ObeOne-KMITL@users.noreply.github.com>
2024-05-13 17:16:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
+templates = Jinja2Templates(directory='templates')
 
 
 
